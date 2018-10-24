77c2698abc067c310f18bc9f3585a7578087cec3
mjparmar <mrpa4248@gmail.com>
2018-10-23 19:54:22 -0500

    changes

--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -35,6 +35,13 @@ class HomePage extends Component {
               <p><a className="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
             </div>
           </div>
+          </div>
+            <div className="col-md-4">
+              <h2>Heading-4</h2>
+              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+              <p><a className="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+          </div>
 
       </div>
       </div>
